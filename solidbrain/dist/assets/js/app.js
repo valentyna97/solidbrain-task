a938b3089e91a3f2a4979a44b0a7488b99871f04
Valentyna Mohort <44020908+valentyna97@users.noreply.github.com>
2020-02-11 14:01:27 +0200

    add-file

---
+++ solidbrain/dist/assets/js/app.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        }, 
+        768:{
+            items: 5
+        }
+    }
+})
+//# sourceMappingURL=app.js.map
